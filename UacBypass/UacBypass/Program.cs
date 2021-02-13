10f9f329f577e27acd7dcad276e9638b3cec4d77
飛田大二郎 <pelo.canvas@gmail.com>
2021-02-13 20:44:25 +0900

    update uacbypass

--- UacBypass/UacBypass/Program.cs
+++ UacBypass/UacBypass/Program.cs
@@ -17,11 +17,21 @@ namespace UacBypass
             var path = "SOFTWARE\\Microsoft\\Windows\\Notepad\\Capabilities";
             RegistryKey key = Registry.LocalMachine.OpenSubKey(path);
             //Console.WriteLine(key.GetValue("ApplicationName"));
-            var names = key.GetValueNames();
-            names?.ToList().ForEach(_ =>
+            var names = key.GetValueNames().ToList();
+            //names?.ToList().ForEach(_ =>
+            //{
+            //    Console.WriteLine(_);
+            //});
+
+            foreach(var (n, idx) in names.Select((n, idx) => (n, idx)))
             {
-                Console.WriteLine(_);
-            });
+                var value = key.GetValue(n);
+                Console.WriteLine($"name:{n}, value:{value}");
+            }
+
+            key = Registry.LocalMachine.CreateSubKey("SOFTWARE\\TestWork\\Test");
+            key.SetValue("TestName", "TestValue");
+
             key.Close();
         }
     }
